c28dec5ee9778af4a0c6bf40f4bff4c6459c6db1
Aditya Vemparala <aditya.vemparala@newfold.com>
2026-09-19 00:55:09 -0500

    New commit

--- stitch/src/components/projectData.js
+++ stitch/src/components/projectData.js
@@ -16,7 +16,7 @@ export const projects = [
     description:
       'AI assistant that uses speech-to-text and NLP summarization for real-time customer support.',
     tags: ['LLMs', 'Streamlit', 'Light-weight RAG', 'Speech-to-Text', 'API', 'Python'],
-    repoUrl: '#',
+    repoUrl: 'https://github.com/vvsadi/SkySolveAI',
     embed: 'https://www.canva.com/design/DAGiexh94gM/uvnlnHe7tRhSBO49cFSL1g/view?embed',
   },
   {
@@ -26,7 +26,7 @@ export const projects = [
     description:
       'Deep analysis of financial datasets using Python and Pandas to identify market trends and correlations.',
     tags: ['Python', 'Pandas', 'Viz'],
-    repoUrl: '#',
+    repoUrl: 'https://github.com/vvsadi/EDA-with-Stock-Time-Series-Data',
   },
   {
     id: 'airsync',
@@ -50,10 +50,18 @@ export const projects = [
   },
   {
     id: 'azure-data-factory-1',
-    title: 'Metadata-Driven Multi-Table Incremental ETL Framework in Azure Data Factory',
+    title: 'Metadata-Driven Multi-Table ETL Pipeline in Azure Data Factory',
     category: 'Personal',
     description: 'Metadata-Driven Multi-Table Incremental ETL Framework in Azure Data Factory supporting Incremental and Full Data Loads. Use Case: \nA company has multiple tables in SQL Server:\n• Customers\n• Orders\n• Products\n\nSteps:\n1. Load all tables to Azure Data Lake Storage Gen2.\n2. Support both Full Load and Incremental Load.\n3. Avoid creating separate pipelines for each table.\n4. Track row counts and execution status.\n5. Automatically update watermark values.',
     tags: ['Data Engineering', 'Azure Data Factory', 'SQL Server', 'Azure Data Lake Storage Gen2'],
+    repoUrl: 'https://github.com/vvsadi/azure_data_eng_proj_ADF',
+  },
+  {
+    id: 'expense-mgmt',
+    title: 'Expense Management Application',
+    category: 'Personal',
+    description: 'Expense management application with features like expense tracking, category management, and reporting."',
+    tags: ['React.js', 'Flask', 'REST API', 'Figma', 'Product Development', 'UI/UX Design', 'DBMS'],
     repoUrl: 'https://github.com/vvsadi/azure_data_eng_proj_ADF',
   },
 ];
